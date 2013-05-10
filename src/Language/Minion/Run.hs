54eec705007d94da388e0454c14b515982c9f8c9
Ozgur Akgun <ozgurakgun@gmail.com>
2013-05-10 16:01:55 +0100

    minion returns multiple solutions by defualt now

--- src/Language/Minion/Run.hs
+++ src/Language/Minion/Run.hs
@@ -9,14 +9,18 @@ import Language.Minion.Definition
 import Language.Minion.Print
 
 
-runMinion :: Model -> IO [(String, Int)]
+runMinion :: Model -> IO [[(String, Int)]]
 runMinion model@(Model _ _ outs) = do
+    let len = length outs
+    let
+        chunk _ [] = []
+        chunk i xs = take i xs : chunk i (drop i xs)
     results <- runMinionPrim (show $ printModel model)
-    return (zip (reverse outs) results)
+    return $ chunk len (zip (cycle $ reverse outs) results)
 
 runMinionPrim :: String -> IO [Int]
 runMinionPrim model = shelly $ silently $ print_stdout False $ do
     setStdin $ T.pack model
-    stdout <- run "minion" ["-printsolsonly", "--"]
+    stdout <- run "minion" ["-findallsols", "-printsolsonly", "--"]
     return $ map (read . T.unpack) $ T.lines stdout
 
